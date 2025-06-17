47bae8a9f7047f203fdeb58fd87c80623e312307
Roxbury Latin <conch.tersest-6y@icloud.com>
2025-06-17 00:20:22 -0400

    Changed header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,8 +1,10 @@
 
-<div class="w-screen h-18 flex justify-between items-center">
-    <img src="/logo/bd.png" alt="The Basecamp logo" class="h-8 pl-2.5" style="margin: 5px;">
-    <div class="mr-3">
-        <button class="border-none text-white mr-2.5 cursor-pointer">log in</button>
-        <button class="h-9 w-20 rounded-md text-white border cursor-pointer">Sign up</button>
+<div class="w-screen h-18 flex justify-between items-end">
+    <img src="/logo/bd.png" alt="The Basecamp logo" class="h-8 pl-4" style="margin: 5px;">
+    <div class="mr-4.5">
+        <button class="text-white mr-3 text-[18px] cursor-pointer h-10 w-19 rounded-lg border-1 border-white">log in</button>
+        <button class="h-10 w-22 text-[18px] rounded-lg border-none text-black bg-white cursor-pointer duration-300 hover:bg-gray-100 hover:shadow-[0_0_26px_rgba(255,255,255,0.5)]" id="signup">Sign up</button>
     </div>
 </div>
+
+
