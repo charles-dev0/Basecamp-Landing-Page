9c10ceb483d4699f49c7e1dd787338b5410ba159
Roxbury Latin <conch.tersest-6y@icloud.com>
2025-06-16 23:23:50 -0400

    Added header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Header from "./Header.svelte";
+</script>
+
+<Header />
+
+<style>
+    :global(body){
+        background-color: black;
+    }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,8 @@
+
+<div class="w-screen h-18 flex justify-between items-center">
+    <img src="/logo/bd.png" alt="The Basecamp logo" class="h-8 pl-2.5" style="margin: 5px;">
+    <div class="mr-3">
+        <button class="border-none text-white mr-2.5 cursor-pointer">log in</button>
+        <button class="h-9 w-20 rounded-md text-white border cursor-pointer">Sign up</button>
+    </div>
+</div>
