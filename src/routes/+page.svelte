c7468e0b7278165193b41707e73dd54ba6d59d37
charles-dev0 <reverb-85.taxes@icloud.com>
2025-06-27 18:18:24 -0400

    Bug Fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
     import Header from "./Header.svelte";
     import Hero from "./Hero.svelte";
+    import { gsap } from "gsap";
+    import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+    gsap.registerPlugin(ScrollTrigger);
 </script>
 
 <Header />
