bf0b510c5d0b646f9157346f8718bf937f9d2e22
charles-dev0 <reverb-85.taxes@icloud.com>
2025-06-30 19:06:03 -0400

    test

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,7 +1,7 @@
 <script>
     import { onMount, onDestroy} from 'svelte';
     import { gsap } from "gsap";
-    import { ScrollTrigger } from 'gsap/ScrollTrigger';
+    import ScrollTrigger from 'gsap/ScrollTrigger';
     gsap.registerPlugin(ScrollTrigger);
 
     
